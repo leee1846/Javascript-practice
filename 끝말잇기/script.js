60925615babb9040a3b3b99ba226179491fd0c43
leee1846 <leee1846@gmail.com>
2020-10-03 21:28:08 +0900

    끝말잇기

---
+++ 끝말잇기/script.js
@@ -0,0 +1,37 @@
+/*끝말잇기
+
+for (let word = "이경준"; true; ) {
+  let answer = prompt(word);
+  if (word[word.length - 1] === answer[0]) {
+    alert("맞았구만");
+    word = answer;
+  } else {
+    alert("틀렸소");
+  }
+}
+*/
+
+
+/*별만들기
+
+for (let i = 1; i <= 5; i++) {
+  console.log("*".repeat(i));
+}
+
+for(let i = 5; i >=1; i-=1) {
+  console.log(' '.repeat(5-i) + '*'.repeat(i))
+}
+
+for (let i = 0; i <=10; i +=2) {
+  console.log("*".repeat(i))
+}
+
+for(i=1; i <=16; i*=2) {
+  console.log("*".repeat(i))
+}
+
+for (let i = 9; i>=1; i -= 2) {
+  console.log(' '.repeat((9-i)/2) + '*'.repeat(i))
+}
+
+*/
